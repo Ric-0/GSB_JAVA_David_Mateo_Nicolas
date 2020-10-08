7589ed566777b3f32ab4f7e6e25b752755e679ce
Ric-0 <70647254+Ric-0@users.noreply.github.com>
2020-10-08 14:12:10 +0200

    Update Localite.java
    test

--- GSB/src/gsb/modele/Localite.java
+++ GSB/src/gsb/modele/Localite.java
@@ -5,7 +5,7 @@ public class Localite {
 	protected String codePostal;
 	protected String ville;
 	
-	
+	// test
 	public Localite(String UnCodePostal,String UneVille){
 		
 		this.codePostal=UnCodePostal;
